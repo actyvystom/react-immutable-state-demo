88c236e3349076fbad3e58cc0e289d7eb1c064bc
actyvystom <thomas.goerldt@actyvyst.com>
2023-03-14 11:24:48 +0100

    demo end with solution options

--- components/Show/index.js
+++ components/Show/index.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import styled from "styled-components";
 import Episode from "../Episode";
 import Season from "../Season";
-
+import { useImmer } from "use-immer";
 const Title = styled.h1`
   text-decoration: underline;
   font-size: 1.7rem;
@@ -19,21 +19,60 @@ const StyledShow = styled.div`
 `;
 
 export default function Show({ initialSeasons = [] }) {
-  const [seasons, setSeasons] = useState(initialSeasons);
+  // const [seasons, setSeasons] = useState(initialSeasons);
+  // Verwendung des useImmer-Hooks
+  const [seasons, updateSeasons] = useImmer(initialSeasons);
 
   function handleToggleHasSeen(seasonNumber, episodeNumber) {
-    setSeasons((prevSeasons) => {
-      const season = prevSeasons.find(({ number }) => number === seasonNumber);
+    // ----- Ursprünglich falsche Mutation des States
+    // setSeasons((prevSeasons) => {
+    //   const season = prevSeasons.find(({ number }) => number === seasonNumber);
+
+    //   const episode = season.episodes.find(
+    //     ({ number }) => number === episodeNumber
+    //   );
+
+    //   episode.hasSeen = !episode.hasSeen;
+
+    //   console.log(prevSeasons);
+
+    //   return prevSeasons;
+    // });
+    // ------ Mutation Ende -------
+    // ----- Korrekte Variante aus der Demo ohne Immer
+    // setSeasons((prevSeason) => {
+    //   return prevSeason.map((season) => {
+    //     if (season.number !== seasonNumber) {
+    //       return season;
+    //     }
+    //     return {
+    //       ...season,
+    //       episodes: season.episodes.map((episode) => {
+    //         // if (episode.number !== episodeNumber) {
+    //         //   return episode;
+    //         // } else {
+    //         //   return {
+    //         //     ...episode,
+    //         //     hasSeen: !episode.hasSeen,
+    //         //   };
+    //         // }
+    //         // Variante mit ternary operator anstatt if...else
+    //         return episode.number !== episodeNumber
+    //           ? episode
+    //           : { ...episode, hasSeen: !episode.hasSeen };
+    //       }),
+    //     };
+    //   });
+    // });
+    // ------ Korrekte Variante Ende --------
+    // ------- Lösung mit Immer
+    updateSeasons((draft) => {
+      const season = draft.find(({ number }) => number === seasonNumber);
 
       const episode = season.episodes.find(
         ({ number }) => number === episodeNumber
       );
-
       episode.hasSeen = !episode.hasSeen;
-
-      console.log(prevSeasons);
-
-      return prevSeasons;
     });
   }
 
